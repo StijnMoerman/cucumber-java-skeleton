0ffadd64830a9362c9368934b58ba378c7902d2d
smoerman <smoerman@sogyo.nl>
2022-12-01 13:51:51 +0100

    bye bye belly

--- gradle/src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ gradle/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -11,40 +11,10 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
 public class StepDefinitions {
-    private Belly belly;
     private Bowl oneBowl;
     private Bowl anotherBowl;
     private Bowl firstBowl;
     private List<Integer> stoneDistributionList = new ArrayList<>();
-
-    @Given("I have {int} cukes in my belly")
-    public void I_have_cukes_in_my_belly(int cukes) {
-        belly = new Belly();
-        belly.eat(cukes);
-    }
-
-    @When("I wait {int} hour(s)")
-    public void I_wait_hours(int hours) {
-        belly.wait(hours);
-    }
-
-    @Then("my belly should {string}")
-    public void My_belly_should_growl(String expected) {
-        if (expected.equals("growl")) {
-            assertTrue(belly.getIsGrowling());
-        }
-        else if (expected.equals("hurt")) {
-            assertTrue(belly.getIsHurting());
-        }
-        else if (expected.equals("feel fine")) {
-            assertFalse(belly.getIsGrowling());
-            assertFalse(belly.getIsHurting());
-        }
-        else {
-            assertFalse(belly.getIsGrowling());
-            assertFalse(belly.getIsHurting());
-        }
-    }
 
     @Given("I have a bowl")
     public void I_have_a_bowl() {

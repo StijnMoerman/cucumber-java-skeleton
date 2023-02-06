d65ea23bf35af9c33cf51e969d3786e1e31b0daf
smoerman <smoerman@sogyo.nl>
2023-02-06 11:10:09 +0100

    Fix dependencies

--- gradle/build.gradle.kts
+++ gradle/build.gradle.kts
@@ -5,10 +5,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation(platform("io.cucumber:cucumber-bom:7.10.1"))
 
-    testImplementation("io.cucumber:cucumber-java")
-    testImplementation("io.cucumber:cucumber-junit-platform-engine")
-    testImplementation("org.junit.platform:junit-platform-suite")
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.cucumber:cucumber-bom:7.5.0")
+    testImplementation("io.cucumber:cucumber-java:7.5.0")
+    testImplementation("io.cucumber:cucumber-junit:7.5.0")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine:7.5.0")
+    testImplementation("org.junit.platform:junit-platform-suite:1.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testImplementation("org.junit.platform:junit-platform-suite-api:1.9.0")
 }
 
 repositories {
